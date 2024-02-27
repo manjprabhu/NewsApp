56f2aec64d07846b4315faf0423e536987539db3
MPB <mp4learning@gmail.com>
2024-02-27 17:00:17 +0530

    Added custom retorift interceptor

---
+++ app/src/main/java/com/mnj/news/interceptor/TokenInterceptor.kt
@@ -0,0 +1,25 @@
+package com.mnj.news.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class TokenInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val response = chain.proceed(originalRequest)
+        // Check if the response indicates that the access token is expired
+        if (response.code == 401) {
+
+            // Call the refresh token API to obtain a new access token
+            val newAccessToken: String = "sdhhgshdf" // get  new access token
+
+            // Create a new request with the updated access token
+            val newRequest = originalRequest.newBuilder()
+                .addHeader("Authorization", "Bearer$newAccessToken")
+                .build()
+
+            // Retry the request with the new access token
+            return chain.proceed(newRequest)
+        }
+    }
+}
